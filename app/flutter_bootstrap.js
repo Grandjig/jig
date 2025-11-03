bda491e0ff21e529b30f63cb7d17d00f1cdf2d5f
Oluwatobi Oyewale <tobioyewale24@gmail.com>
2025-11-03 11:53:13 +0100

    Deploy J!G web app

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "342882262"
+    serviceWorkerVersion: "3464059453"
   }
 });
